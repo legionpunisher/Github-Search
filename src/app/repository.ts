a68d425010867cd3c3ec0bd6f5d859c36b6f82c1
legionpunisher <mbuguamoses656@gmail.com>
2020-09-01 11:07:45 +0300

    created a constructor on repository and assigned it values

--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,11 @@
 export class Repository {
+    constructor(
+        public name:string,
+        public owner: any,       
+        public login : string,
+        public avatar_url: string,
+        public following  : string,
+        public repos_url : any,
+        public url : string,
+    ){}
 }
