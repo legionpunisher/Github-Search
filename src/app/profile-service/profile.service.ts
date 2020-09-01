52774cba94665d377554d93792b2ac84e440180a
legionpunisher <mbuguamoses656@gmail.com>
2020-09-01 11:25:40 +0300

    imported class repohub and the userhub

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -3,6 +3,9 @@ import {Repository} from '../repository';
 import {User} from '../user';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
+import {Userhub} from '../userhub';
+import {Repohub}from '../repohub';
+
 
 @Injectable({
   providedIn: 'root'
