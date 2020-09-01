0156b870a4ef9cafc767a9f9a90641c69ef15203
legionpunisher <mbuguamoses656@gmail.com>
2020-09-01 11:22:20 +0300

    imported the http client and the environment

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Repository} from '../repository';
 import {User} from '../user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
