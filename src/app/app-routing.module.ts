e4c9b40f5193a11da414fb3c4e263231f7fe2664
legionpunisher <mbuguamoses656@gmail.com>
2020-09-01 13:00:10 +0300

    created a path for the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ProfileService} from './profile-service/profile.service';
+import {Userhub} from './userhub';
+import { UserInfoComponent } from './user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'profiles', component:  UserInfoComponent},
+  { path: '', redirectTo:"/profiles", pathMatch:"full"},
+  { path:'**', component:UserInfoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
